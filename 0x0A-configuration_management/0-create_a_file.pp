711d265fda04228ed9d82e794dd8673c65a27e21
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-10 00:09:15 -0800

    Create file with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+-- comment
+file {'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  owner   => 'www-data',
+  mode    => '0744',
+  path    => '/tmp/school',
+}
+
