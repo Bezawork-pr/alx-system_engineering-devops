6bf0bcfb1efe8430cb6be6d6adba1ad9f5cef1b5
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-11 03:41:57 -0800

    Change ensure to present just incase it is removing file and not line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Use puppet to configure SSH client to use our private key
 # And refuse password authentication
 file_line {'Disable password authentication':
-  ensure => 'absent',
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'BatchMode yes',
 }
 file_line {'Find ssh private key':
-  ensure => 'absent',
+  ensure => 'present',
   path   =>'/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
 }
