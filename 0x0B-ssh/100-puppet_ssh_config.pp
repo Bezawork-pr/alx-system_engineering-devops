b0fab30b3262bb78d4782fb991f42050e0b62f8f
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-11 03:40:28 -0800

    Remove match to find line to change

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,12 +3,10 @@
 file_line {'Disable password authentication':
   ensure => 'absent',
   path   => '/etc/ssh/ssh_config',
-  match  => 'BatchMode*.',
   line   => 'BatchMode yes',
 }
 file_line {'Find ssh private key':
   ensure => 'absent',
   path   =>'/etc/ssh/ssh_config',
-  match  => 'IdentityFile*.',
   line   => 'IdentityFile ~/.ssh/school',
 }
