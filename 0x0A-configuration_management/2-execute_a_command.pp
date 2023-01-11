6f20aa717f325558280e4d2b86d2b559bd6f48f6
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-10 22:28:06 -0800

    Change title to the command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow
-exec {'pkill':
+exec {'pkill -15 killmenow':
   command => 'pkill -15 killmenow',
   path =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
