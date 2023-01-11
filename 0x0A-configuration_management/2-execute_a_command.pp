b6bd37987cc0ec8e4aff3b9874aeac1a0baa0255
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-10 22:29:59 -0800

    Change title to file name to be killed

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow
-exec {'pkill -15 killmenow':
+exec {'killmenow':
   command => 'pkill -15 killmenow',
   path =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
