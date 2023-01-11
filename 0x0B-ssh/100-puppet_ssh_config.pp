be91236b278c50179d2c91eb46b6be9f5a9b7887
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-11 04:00:44 -0800

    Add necessary spaces

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Use puppet to configure SSH client to use our private key
 # And refuse password authentication
-file_line {'Disable password authentication':
+file_line { 'Disable password authentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '  BatchMode yes',
 }
-file_line {'Find ssh private key':
+file_line { 'Find ssh private key':
   ensure => 'present',
   path   =>'/etc/ssh/ssh_config',
   line   => '  IdentityFile ~/.ssh/school',
