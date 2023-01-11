096f2e3868ba74f6167c05ebdd86abe2bfd3b932
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-10 22:34:36 -0800

    Fix alignment and add comma between curly braces

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow
-exec {'killmenow':
+exec { 'killmenow':
   command => 'pkill -15 killmenow',
-  path =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+  path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
