e94f874aaf49a15dd6c1bd779bdc66cab52c7d3e
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-13 05:36:20 -0800

    Remove trailing white space

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,19 +1,19 @@
 #  Install Nginx web server (w/ Puppet)
-exec {'sudo apt update': 
+exec {'sudo apt update':
   command => 'sudo apt-get -y update',
   path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
 exec {'install nginx':
   command => 'sudo apt-get -y install nginx',
-  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],  
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
 exec {'sudo ufw allow "Nginx HTTP"':
   command => 'sudo ufw allow "Nginx HTTP"',
-  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],  
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
 exec {'update index.nginx-debian.html':
   command => 'sudo echo "Hello World!" |  /var/www/html/index.nginx-debian.html',
-  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],  
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
 exec {'Edit /etc/nginx/sites-available/default':
   command => 'sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
