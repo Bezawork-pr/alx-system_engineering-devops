edbba5a60738acc65eaedbfc1c05ee3199e0920b
Bezawork B Lindlof <107026397+Bezawork-pr@users.noreply.github.com>
2023-01-13 17:11:56 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,7 +3,4 @@ exec {'/usr/bin/env sudo apt-get update -y': }
 exec {'/usr/bin/env sudo apt-get install -y nginx': }
 exec {'/usr/bin/env sudo echo "Hello World!" |  /var/www/html/index.nginx-debian.html': }
 exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
-exec {'/usr/bin/env sudo vim /var/www/html/error-404.html': }
-exec {'/usr/bin/env echo "Ceci n'est pas une page" > /var/www/html/error-404.html': }
-exec {'/usr/bin/env sed -i "/http {/ a\\\terror_page 404 /error-404.html;" /etc/nginx/nginx.conf': }
 exec {'/usr/bin/env service nginx start': }
