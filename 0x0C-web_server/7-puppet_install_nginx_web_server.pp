7873b6ae43a955c0c8f20d0d64c28300122c286e
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-13 05:12:00 -0800

    Edit syntax error on puppet file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,7 +2,9 @@
 exec {'sudo apt-get -y update': }
 exec {'sudo apt-get -y install nginx': }
 exec {'sudo ufw allow "Nginx HTTP"': }
-exec {'echo "Hello World!" > /var/www/html/index.html': }
-exec {'sed -i '/listen 80 default_server;/a rewrite ˆ/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' /etc/nginx/sites-available/default': }
-exec {'sudo nano /usr/share/nginx/html/custom_404.html': }
-exec {'echo "Ceci n'est pas une page" > /usr/share/nginx/html/custom_404.html': }
+exec {'sudo echo "Hello World!" |  /var/www/html/index.nginx-debian.html': }
+exec {'sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
+exec {'sudo vim /var/www/html/error-404.html': }
+exec {'echo "Ceci n\'est pas une page" > /var/www/html/error-404.html': }
+exec {'sed -i "/http {/ a\\\terror_page 404 /error-404.html;" /etc/nginx/nginx.conf': }
+exec {'sudo service nginx restart': }
