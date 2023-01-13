376b0190039cd318aed2c8bda2ca09d88bd6a09b
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-13 06:36:33 -0800

    Remove double quotes from echo and pipe

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,6 +2,12 @@
 package { 'nginx':
   provider => 'apt',
 }
-exec {'/usr/bin/sudo echo "Hello World!" |  /var/www/html/index.nginx-debian.html': }
-exec {'/usr/bin/sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
-exec {'/usr/bin/sudo service nginx start': }
+exec {'modify_html_file':
+  command => '/usr/bin/sudo /bin/echo Hello World! >  /var/www/html/index.nginx-debian.html',
+}
+exec {'redirect':
+  command => '/usr/bin/sudo /bin/sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+}
+exec {'reload':
+  command => '/usr/bin/sudo /usr/sbin/service nginx start',
+}
