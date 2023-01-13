9f1b34a75e5573e22554e2749c3495e42cf91a6e
Bezawork B Lindlof <107026397+Bezawork-pr@users.noreply.github.com>
2023-01-13 17:43:27 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,7 +6,7 @@ exec {'modify_html_file':
   command => '/usr/bin/sudo /bin/echo Hello World! >  /var/www/html/index.nginx-debian.html',
 }
 exec {'redirect':
-  command => '/usr/bin/sudo /bin/sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  command => '/usr/bin/sudo /bin/sed -i "66i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
 }
 exec {'reload':
   command => '/usr/bin/sudo /usr/sbin/service nginx start',
