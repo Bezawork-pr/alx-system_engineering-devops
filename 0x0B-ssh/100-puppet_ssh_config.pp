345895e605271f871c9758ad288a4a683288ef4c
Bezawork B Lindlof <107026397+Bezawork-pr@users.noreply.github.com>
2023-01-13 18:20:25 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 # configure ssh_config to use the private key ~/.ssh/school using puppet
-# configure ssh_config to refuse to authenticate using a password using puppet
 include stdlib
+
 file_line { 'Disable_password_authentication':
   path    => '/etc/ssh/ssh_config',
   line    => 'BatchMode yes',
   replace => true,
 }
+
 file_line { 'Use_private_key':
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
