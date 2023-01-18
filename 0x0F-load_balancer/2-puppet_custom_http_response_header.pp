3f66e03dc0471963c8e9e34db73904c27553ed23
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-18 08:01:05 -0800

    Do task 0 with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+#  Install Nginx web server (w/ Puppet)
+package { 'nginx':
+  provider => 'apt',
+}
+exec {'modify_html_file':
+  command => '/usr/bin/sudo /bin/echo Hello World! >  /var/www/html/index.nginx-debian.html',
+}
+exec {'redirect':
+  command => '/usr/bin/sudo /bin/sed -i "66i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+}
+exec {'Add_header':
+  command => '/usr/bin/sudo /bin/sed -i  "/server {/a\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+}
+exec {'reload':
+  command => '/usr/bin/sudo /usr/sbin/service nginx start',
+}
