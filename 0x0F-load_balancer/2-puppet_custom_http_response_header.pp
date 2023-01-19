548daf1f8ee785130ea3a0e711fbf020b4abd290
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-19 07:26:35 -0800

    Applu all commands together

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,8 @@
-#  Install Nginx web server (w/ Puppet)
-package { 'nginx':
-  provider => 'apt',
-}
-exec {'modify_html_file':
-  command => '/usr/bin/sudo /bin/echo Hello World! >  /var/www/html/index.nginx-debian.html',
-}
-exec {'redirect':
-  command => '/usr/bin/sudo /bin/sed -i "66i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-}
-exec {'Add_header':
-  command => '/usr/bin/sudo /bin/sed -i  "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
-}
-exec {'reload':
-  command => '/usr/bin/sudo /usr/sbin/service nginx start',
+#!/usr/bin/env bash
+# install nginx add header, 404 page and redirect
+exec {'Installation':
+  command => 'sudo apt-get update -y;
+    sudo apt-get install nginx -y;
+    sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-availble/default;
+    sudo service nginx restart',
 }
