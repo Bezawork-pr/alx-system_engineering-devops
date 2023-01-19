08686e53d06ff6cbee95936b90944df6f3de41a1
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-19 07:16:26 -0800

    Fix error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,7 +9,7 @@ exec {'redirect':
   command => '/usr/bin/sudo /bin/sed -i "66i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
 }
 exec {'Add_header':
-  command => '/usr/bin/sudo /bin/sed -i  "/server {/a\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+  command => '/usr/bin/sudo /bin/sed -i  "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
 }
 exec {'reload':
   command => '/usr/bin/sudo /usr/sbin/service nginx start',
