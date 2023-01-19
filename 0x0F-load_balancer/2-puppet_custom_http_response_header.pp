8ba210a841466394a7765f67f0e48655a38fd79c
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-19 08:03:49 -0800

    Write a script to install haproxy and configure

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# install nginx add header, 404 page and redirect
-exec {'Installation':
-  command => 'sudo apt-get update -y;
-    sudo apt-get install nginx -y;
-    sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-availble/default;
-    sudo service nginx restart',
+#install nginx add header, 404 page and redirect
+exec {'Update':
+  command  => 'sudo apt-get update',
+  provider => present,
 }
+package {'nginx':
+  ensure => present,
+}
+file_line {'file':
+  ensure => present,
+  path }
