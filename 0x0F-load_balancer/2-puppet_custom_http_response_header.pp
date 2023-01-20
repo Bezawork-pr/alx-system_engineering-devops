51feca30f98e1c26cb205e7efde6f4b779475be3
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-20 04:09:00 -0800

    Write all command in one line

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 #install nginx add header, 404 page and redirect
-exec {'Update':
-  command  => 'sudo apt-get update',
-  provider => present,
+exec {'Header':
+  command  => 'apt-get -y update && apt-get -y install nginx && echo "Hello World!" > /var/www/html/index.nginx-debian.html && sed -i  "/server {/a\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default && service nginx restart',
+  provider => shell,
 }
-package {'nginx':
-  ensure => present,
-}
-file_line {'file':
-  ensure => present,
-  path }
