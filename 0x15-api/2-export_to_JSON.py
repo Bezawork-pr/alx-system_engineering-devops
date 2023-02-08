edf6f1741176685be648a0f0fa504d466d998ff0
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-08 05:16:35 -0800

    Set dictionary 2 to empty to rewrite and append to list

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -17,16 +17,18 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users"
     employee = requests.get("{}/{}".format(url, employee_id)).json()
     todos = requests.get("{}/{}/todos".format(url, employee_id)).json()
+    print(todos)
     employee_name = employee['name']
     user_name = employee['username']
     dict_1 = {}
     dict_2 = {}
     my_list = []
-    for i in range(len(todos)):
-        dict_2['task'] = todos[i].get('title')
-        dict_2['completed'] = todos[i].get('completed')
+    for i in todos:
+        dict_2['task'] = i.get('title')
+        dict_2['completed'] = i.get('completed')
         dict_2['username'] = user_name
         my_list.append(dict_2)
+        dict_2 = {}
     dict_1[argv[1]] = my_list
     with open("{}.json".format(argv[1]), "w") as jsonfile:
         json.dump(dict_1, jsonfile)
