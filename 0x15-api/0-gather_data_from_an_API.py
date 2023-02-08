bb13db49c6fb93bb07aaf552ba96d91577a88044
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-08 02:47:22 -0800

    Execute when main and during importation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,20 +7,21 @@ his/her TODO list progress.
 from sys import argv
 import requests
 
-employee_id = argv[1]
-total_task = 0
-completed_task = 0
-url = "https://jsonplaceholder.typicode.com/users"
-employee = requests.get("{}/{}".format(url, employee_id)).json()
-todos = requests.get("{}/{}/todos".format(url, employee_id)).json()
-employee_name = employee['name']
-for i in range(len(todos)):
-    total_task = total_task + 1
-    if todos[i]['completed'] is True:
-        completed_task = completed_task + 1
-string = "Employee {} is done with tasks({}/{})".format(
-                employee_name, completed_task, total_task)
-print(string)
-for i in range(len(todos)):
-    if todos[i]['completed'] is True:
-        print("\t{}".format(todos[i]['title']))
+if __name__ == "__main__":
+    employee_id = argv[1]
+    total_task = 0
+    completed_task = 0
+    url = "https://jsonplaceholder.typicode.com/users"
+    employee = requests.get("{}/{}".format(url, employee_id)).json()
+    todos = requests.get("{}/{}/todos".format(url, employee_id)).json()
+    employee_name = employee['name']
+    for i in range(len(todos)):
+        total_task = total_task + 1
+        if todos[i]['completed'] is True:
+            completed_task = completed_task + 1
+    string = "Employee {} is done with tasks({}/{})".format(
+                    employee_name, completed_task, total_task)
+    print(string)
+    for i in range(len(todos)):
+        if todos[i]['completed'] is True:
+            print("\t{}".format(todos[i]['title']))
