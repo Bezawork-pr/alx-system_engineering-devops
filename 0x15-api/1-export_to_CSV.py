cdb297269a5c787f5ec87f18db3c027ea1cf4c36
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-08 02:00:24 -0800

    Add more documentation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""Using what you did in the task #0,
-extend your Python script to export data in the CSV format."""
+"""
+Using what you did in the task #0,
+extend your Python script to export data in the CSV format.
+Records all tasks that are owned by this employee
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+"""
 import requests
 from sys import argv
 import csv
