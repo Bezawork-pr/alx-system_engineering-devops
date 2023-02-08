4f49e309eff4c376c5584b289cd66983bb316af4
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-08 02:03:00 -0800

    Order imports alphabetically

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,9 +6,9 @@ Records all tasks that are owned by this employee
 Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
 File name must be: USER_ID.csv
 """
-import requests
 from sys import argv
 import csv
+import requests
 
 if __name__ == "__main__":
     employee_id = argv[1]
