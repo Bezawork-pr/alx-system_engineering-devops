f4e4976aed0b55aa9527969b3357289ecd4be886
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-08 05:44:06 -0800

    Get all employees task, username and task completion status on json file

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0,
+extend your Python script to export data in the JSON format
+of all the employees
+Write a Python script that, using this REST API,
+for a given employee ID, returns information about
+his/her TODO list progress.
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    total_task = 0
+    completed_task = 0
+    url = "https://jsonplaceholder.typicode.com"
+    employees = requests.get("{}/users".format(url)).json()
+    todos = requests.get("{}/todos".format(url)).json()
+    dict_1 = {}
+    dict_2 = {}
+    my_list = []
+    for employee in employees:
+        for task in todos:
+            if employee.get('id') == task.get('userId'):
+                dict_2['username'] = employee.get('username')
+                dict_2['task'] = task.get('title')
+                dict_2['completed'] = task.get('completed')
+                my_list.append(dict_2)
+                dict_2 = {}
+        dict_1[employee.get('id')] = my_list
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(dict_1, jsonfile)
