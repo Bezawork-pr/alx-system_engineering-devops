bb63f1b38c825a687dae8da4a2b29774985ddadc
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-08 03:11:46 -0800

    Import libraries alphabetically

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,8 +4,8 @@ Write a Python script that, using this REST API,
 for a given employee ID, returns information about
 his/her TODO list progress.
 """
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     employee_id = argv[1]
