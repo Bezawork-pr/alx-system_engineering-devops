43b4b15d903428eae34ee10523f5143829db8d43
Bezawork-pr <bezaworkalx@gmail.com>
2023-03-02 06:04:28 -0800

    And path to command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 #Debugging mode by changing WP_DEBUG to true run curl -4 to find error-> typo error
 exec { 'Fixemptyresponse':
-  command => 'sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php',
-  path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  command => '/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php',
 }
