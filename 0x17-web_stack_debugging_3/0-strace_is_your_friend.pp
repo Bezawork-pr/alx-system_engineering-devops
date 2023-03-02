ddb9327f381b5b72b2b2dd479de7e096775b2509
Bezawork-pr <bezaworkalx@gmail.com>
2023-03-02 06:06:35 -0800

    Remove quots

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 #Debugging mode by changing WP_DEBUG to true run curl -4 to find error-> typo error
 exec { 'Fixemptyresponse':
-  command => '/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php',
+  command => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 }
